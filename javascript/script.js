426c01fff72f6888a0c1c33264e6b378bbd13174
gabrielnascy <gabrielhenrique8545301@gmail.com>
2026-03-04 18:24:07 -0300

    feat: algumas alterações na landing page

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('#mobile_btn').on('click', function () {
-        $('#mobile_menu').toogleClass('active');
-        $('#mobile_btn').find('i').toogleClass('fa-x');
-    })
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
 
     const sections = $('section');
-    const navItems = $('.nav_item');
+    const navItems = $('.nav-item');
 
     $(window).on('scroll', function () {
         const header = $('header');
@@ -16,25 +16,22 @@ $(document).ready(function () {
         if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
-            header.css('box-shadow', '5px 1px 5px (0, 0, 0, 0.1');
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1');
         }
 
-        sections.each(function (i) {
+        sections.each(function(i) {
             const section = $(this);
             const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop + section.outerHeight();
+            const sectionBottom = sectionTop+ section.outerHeight();
 
-            if (scrollPosition >= sectionTop && scrollPosition < screenBottom) {
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
                 return false;
             }
-
-
         })
 
         navItems.removeClass('active');
         $(navItems[activeSectionIndex]).addClass('active');
-
     });
 
     ScrollReveal().reveal('#cta', {
@@ -49,16 +46,15 @@ $(document).ready(function () {
         distance: '20%'
     });
 
-    ScrollReveal().reveal('#testimonials_chef', {
+    ScrollReveal().reveal('#testimonial_chef', {
         origin: 'left',
         duration: 1000,
         distance: '20%'
-    });
+    })
 
     ScrollReveal().reveal('.feedback', {
         origin: 'right',
         duration: 1000,
         distance: '20%'
-    });
-
-}); 
+    })
+});
